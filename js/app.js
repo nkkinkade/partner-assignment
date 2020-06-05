09d929e800fb504381c5431f901d60d98cfa692c
Nathan Kinkade <nkkinkade@gmail.com>
2020-06-04 23:13:48 -0500

    add loops

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
 'use strict';
-console.log('This file connected!');
 
 function firstTest() {
 
     var today = new Date();
     var hourNow = today.getHours();
     var userName = prompt('What is your name?');
-    // var quest = prompt('What is your quest?');
-    // var swallow = prompt('What is the air-speed velocity of an unladen swallow?');
     var greeting;
 
     if (hourNow > 18) {
@@ -23,6 +20,44 @@ function firstTest() {
     }
     document.getElementById('greeting').innerHTML = greeting;
 }
+firstTest();
 
- firstTest();
+var getMusic = function() {
+    var music = prompt('What is your favorite music genre?').toLowerCase().trim();
+    var item;
+    while (music !== 'rock' && music !== 'rap' && music !== 'country' && music !== 'classical' && music !== 'pop') {
+      music = prompt('Please enter "rock," "rap," "country" or "pop"...').toLowerCase().trim();
+    }
+    if (music === 'rock') {
+      item = '<img src="images/rock.jpg">';
+    } else if (music === 'rap') {
+      item = '<img src="images/rap.jpg">';
+    } else if (music === 'country') {
+      item = '<img src="country.jpg">';
+    } else if (music === 'pop') {
+      item = '<img src="images/pop.png">';
+    }
+    
+    return item; 
+};
 
+var howMany = function() {
+    var age = prompt('How many years has this been your favorite?').trim();
+    while( age === '' || isNaN(age) ) {
+      age = prompt('Please enter a number. How many years has this been your favorite?').trim();
+    }
+    return Number(age);
+};
+
+var showMusic = function() {
+    var item = getMusic();
+    var total = howMany();
+    var result = '';
+    //use this to make the number of house images display on the screen. 
+    for(var i = 1; i <= total; i++) {
+      result = result + '<p>' + item + '</p>';
+    }
+    return result;
+};
+
+  document.getElementById('showMusic').innerHTML = showMusic();
